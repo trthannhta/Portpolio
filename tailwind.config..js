9ca6df5c5486ea5bc7372df99727b07da6697f9b
trthannhta <tatrungthanh1690@gmail.com>
2024-04-22 21:28:13 +0700

    port

---
+++ tailwind.config..js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+      colors: {
+        'primary-lighter': '#F2F2FE',
+      },
+      fontFamily: {
+        sans: ['Graphik', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+      extend: {
+        spacing: {
+          '8xl': '96rem',
+          '9xl': '128rem',
+        },
+        borderRadius: {
+          '4xl': '2rem',
+        }
+      }
+    },
+  }
